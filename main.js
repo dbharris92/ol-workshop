a3fb365b1f41469f18698be92c989346f7ec09f3
dbharris <harris.db92@gmail.com>
2018-09-10 16:44:47 -0400

    drag and drop

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 alert('Hello Workshop');
 import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZSource from 'ol/source/XYZ';
+import sync from 'ol-hashed';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
-new Map({
+
+const source = new VectorSource();
+
+// [!map]
+const map = new Map({
   target: 'map-container',
-  layers: [
-    new TileLayer({
-      source: new XYZSource({
-        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
-      })
-    })
-  ],
   view: new View({
     center: [0, 0],
     zoom: 2
   })
 });
+// [!map]
+
+const layer = new VectorLayer({
+  source: source
+});
+map.addLayer(layer);
+
+map.addInteraction(new DragAndDrop({
+  source: source,
+  formatConstructors: [GeoJSON]
+}));
+
